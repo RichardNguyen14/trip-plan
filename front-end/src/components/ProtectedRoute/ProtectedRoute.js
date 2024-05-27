3e476e532f33fe41def997af650a8c9336a896d1
Richard <nguyenhuuvien.devfe@gmail.com>
2024-05-27 18:40:26 +0700

    123

--- front-end/src/components/ProtectedRoute/ProtectedRoute.js
+++ front-end/src/components/ProtectedRoute/ProtectedRoute.js
@@ -3,7 +3,7 @@ import { Navigate, Outlet } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
   const accessToken = localStorage.getItem("accessToken");
-  console.log("hello");
+
   if (!accessToken) {
     // Nếu không có accessToken, chuyển hướng đến trang đăng nhập
     return <Navigate to="/login" replace />;
